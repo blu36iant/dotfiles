4470b5541207667ab92f6ee613c3b2c457dcbe48
Germán Correa <germcb@proton.me>
2023-07-31 20:44:33 -0300

    add p10k-portable.zsh prompt config for text console, minor fix to init.sh

--- init.sh
+++ init.sh
@@ -9,7 +9,6 @@ stow zsh
 #git clone --branch release-v1 https://github.com/zap-zsh/zap.git "$HOME/.config/zsh/zap"
 #source $HOME/.zshrc 
 zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1 --keep
-source "$HOME/.zshrc"
 
 stow river
 stow foot
@@ -21,7 +20,7 @@ echo "Installing getnf for NerdFonts"
 [[ -d "$HOME/.local/bin" ]] || mkdir -p "$HOME/.local/bin"
 curl 'https://raw.githubusercontent.com/ronniedroid/getnf/master/getnf' -o "$HOME/.local/bin/getnf"
 chmod u+x $HOME/.local/bin/getnf
-getnf<<EOF
+$HOME/.local/bin/getnf<<EOF
 8,13,17,18,21,27,28,30,31,32,34,41,47,51
 
 EOF
