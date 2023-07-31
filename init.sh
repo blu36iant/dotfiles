7eae20eb2401d08a321049f0e6541058398578ce
Germán Correa <germcb@proton.me>
2023-07-31 17:55:04 -0300

    stow missing lines in init.sh

--- init.sh
+++ init.sh
@@ -6,15 +6,19 @@ echo "Installing necesary distro packages..."
 sudo xbps-install -Syu $VOID_PKGS 
 
 echo "Installing Zsh plugin manager...."
+stow git
 stow zsh
 git clone -b "--release-v1" https://github.com/zap-zsh/zap.git "$HOME/.config/zsh/zap"
 source $HOME/.zshrc
 #zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1 --keep
 
 stow river
+stow foot
 stow yambar
+stow swappy
+stow sway
 
 echo "Installing getnf for NerdFonts"
 [[ -d "$HOME/.local/bin" ]] || mkdir -p "$HOME/.local/bin"
 curl 'https://raw.githubusercontent.com/ronniedroid/getnf/master/getnf' -o "$HOME/.local/bin/getnf"
-
+getnf
