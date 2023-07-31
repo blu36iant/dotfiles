b11b92a3c0bc4487a8d00b3c57a44acc2c65e7ae
Germán Correa <germcb@proton.me>
2023-07-31 18:11:34 -0300

    fixing more errors in init.zsh

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-#/bin/sh
+#! /bin/zsh
 
 VOID_PKGS='git stow curl neovim zsh zsh-completions zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting bat fzf fd ripgrep'
 
@@ -8,9 +8,7 @@ sudo xbps-install -Syu $VOID_PKGS
 echo "Installing Zsh plugin manager...."
 stow git
 stow zsh
-git clone -b "release-v1" https://github.com/zap-zsh/zap.git "$HOME/.config/zsh/zap"
-zsh
-#zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1 --keep
+zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1 --keep
 
 stow river
 stow foot
@@ -21,4 +19,5 @@ stow sway
 echo "Installing getnf for NerdFonts"
 [[ -d "$HOME/.local/bin" ]] || mkdir -p "$HOME/.local/bin"
 curl 'https://raw.githubusercontent.com/ronniedroid/getnf/master/getnf' -o "$HOME/.local/bin/getnf"
+export PATH=$PATH:$HOME/.local/bin
 getnf
