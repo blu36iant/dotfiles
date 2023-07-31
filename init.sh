6080ed64b66a0a968094102ca4026cf1777e1d4d
Germán Correa <germcb@proton.me>
2023-07-31 20:25:03 -0300

    source zsh after installing zap for being in a subshell

--- init.sh
+++ init.sh
@@ -9,6 +9,7 @@ stow zsh
 #git clone --branch release-v1 https://github.com/zap-zsh/zap.git "$HOME/.config/zsh/zap"
 #source $HOME/.zshrc 
 zsh <(curl -s https://raw.githubusercontent.com/zap-zsh/zap/master/install.zsh) --branch release-v1 --keep
+source "$HOME/.zshrc"
 
 stow river
 stow foot
